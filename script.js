550ac8cbd2b553bf179e1d17641a7a446139d024
AR-EM <178697567+AR-EM@users.noreply.github.com>
2025-07-29 19:38:14 +0530

    Made all basic working of the calculator. Bugs yet to fix.

--- script.js
+++ script.js
@@ -1,32 +1,87 @@
-function add(a, b){
-    return a+b;
+function add(a, b) {
+  return a+b;
 }
 
-function subtract(a, b){
-    return a-b;
+function subtract(a, b) {
+  return a-b;
 }
 
-function multiply(a, b){
-    return a*b;
+function multiply(a, b) {
+  return a*b;
 }
 
-function divide(a, b){
-    return a/b;
+function divide(a, b) {
+  if (b == 0) return "Nah Bro";
+  return a/b;
 }
 
-function operate(a, operator, b){
-    if (operator == "+" ){
-        add(a,b);
+function operating(num1, operator, num2) {
+  const n1 = parseFloat(num1);
+  const n2 = parseFloat(num2);
+  switch (operator) {
+    case "+":
+      return add(n1, n2);
+    case "-":
+      return subtract(n1, n2);
+    case "*":
+      return multiply(n1, n2);
+    case "/":
+      return divide(n1, n2);
+  }
+}
+
+function reset(){
+  num1 = '';
+  num2 = '';
+  currentOperator = null;
+  display.textContent = '';
+}
+
+
+let num1 = '';
+let num2 = '';
+let currentOperator = null;
+
+const digits = document.querySelectorAll(".digit");
+const display = document.querySelector(".display");
+const operation = document.querySelectorAll(".operation");
+
+display.textContent = "";
+
+digits.forEach((number) => {
+  number.addEventListener("click", () => {
+    if (currentOperator === null) {
+      num1 += number.textContent;
+      display.textContent = num1;
+    } else {
+      num2 += number.textContent;
+      display.textContent = num2;
     }
-    else if (operator == "-" ){
-        subtract(a,b);
-    }
-    else if (operator == "*" ){
-        multiply(a,b);
-    }
-    else if (operator == "/" ){
-        divide(a,b);
-    }
-}
+  });
+});
 
-let a, operator, b;
+operation.forEach((operate) => {
+  const opText = operate.textContent;
+  operate.addEventListener("click", () => {
+    if (operate.textContent == "AC") {
+      reset();
+    }
+    if (operate.textContent == "=") {
+      const result = operating(num1, currentOperator, num2);
+      display.textContent = result;
+      num1 = result.toString();
+      num2 = '';
+      currentOperator = null;
+    }
+    if (currentOperator && num1 && num2){
+      const result = operate(num1,currentOperator, num2);
+      display.textContent = result;
+      num1 = result.toString();
+      num2 = '';
+      currentOperator = null;
+    }
+  currentOperator = opText;
+  });
+});
+
+reset();
